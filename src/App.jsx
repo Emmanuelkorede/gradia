23f704a5b19f0b4c66ae4ac0cf699b7c11e98103
Emmanuelkorede <emmanueljob2009@gmail.com>
2026-05-08 01:08:37 +0100

    starting files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Routes , Route} from 'react-router'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Routes>
+      
+    </Routes>
+    </>
+  )
+}
+
+export default App
